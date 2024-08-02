3b01108af5ae52199335c3e85ae48ef4e88c1c85
Sunil Vakotar <sunilvakotar@gmail.com>
2024-08-02 12:49:10 +0400

    Initial Commit

---
+++ BarcodeDetection/BarcodeDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeDetectionApp.swift
+//  BarcodeDetection
+//
+//  Created by Sunil Vakotar on 02/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
